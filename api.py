faee209f724562a6cfe6575c9c1341f96afbb469
eqbrown_teacher <nobody@makingwithcode.org>
2025-08-28 08:48:20 +0800

    update method for getting payload

--- api.py
+++ api.py
@@ -22,22 +22,28 @@ def api_all_riddles():
 
 @app.route(f'/{BASE_URL}/one', methods=['GET'])
 def api_one_riddle():
+    if 'id' not in  request.args:
+        return {'error': 'id is required.'}, 404 
+    
     # get API parameters 
-    data = request.get_json()
-    id = data.get('id')
+    id = request.args['id']
+
+    # get riddle from db
     riddle = get_one_riddle(id)
    
     # error handeling
     if riddle is None:
-        return {'error': 'Riddle not found'}, 404
+        return jsonify({'error': 'Riddle not found'}), 404
     
     return jsonify({'riddle':json_riddle_answerless(riddle)}), 200
 
 @app.route(f'/{BASE_URL}/difficulty', methods=['GET'])
 def api_difficulty_riddle():
+    if 'id' not in  request.args:
+        return {'error': 'id is required.'}, 404 
+
     # get API parameters 
-    data = request.get_json()
-    id = data.get('id')
+    id = request.args['id']
     riddle = get_one_riddle(id)
 
     # error handeling
@@ -59,14 +65,14 @@ def api_random_riddle():
 
 @app.route(f'/{BASE_URL}/new', methods=['POST'])
 def api_new_riddle():
-    # get API parameters 
-    data = request.get_json()
-    question = data.get('question')
-    answer = data.get('answer')
-
     # error handeling
-    if not question or not answer:
-        return {'error': 'Question and Answer are required.'}, 404
+    if 'question' not in  request.args or 'answer' not in request.args:
+        return {'error': 'question and answer are required.'}, 404 
+    
+    # get API parameters 
+    question = request.args['question']
+    answer = request.args['answer']
+
 
     riddle = new_riddle(question, answer)
     
@@ -77,23 +83,19 @@ def api_new_riddle():
 @app.route(f'/{BASE_URL}/guess', methods=['PUT'])
 def api_guess_riddle():
     # get API parameters 
-    data = request.get_json()
-    id = data.get('id')
-    guess = data.get('guess')
 
     # error handeling
-    if not id or not guess:
+    if 'id' not in  request.args or 'guess' not in request.args:
         return {'error': 'id and guess are required.'}, 404 
+    
+    # get id and guess
+    id = request.args['id']
+    guess = request.args['guess']
 
     riddle = get_one_riddle(id)
 
     if riddle is None:
         return {'error': 'Riddle not found'}, 404
-
-    print(riddle['id'])
-
-    # return {'r':0}
-
 
     # error handeling
     if riddle is None:
@@ -103,12 +105,10 @@ def api_guess_riddle():
         riddle = update_riddle_stats(id,True)
         return {'correct': True, 'riddle': json_riddle(riddle)}, 200
     
-#    return {'r':0}
     else:
         print(id)
         riddle = update_riddle_stats(id,False)
         return {'correct': False, 'riddle': json_riddle_answerless(riddle)}, 200
-        # return {'r':0}
 
 
 
